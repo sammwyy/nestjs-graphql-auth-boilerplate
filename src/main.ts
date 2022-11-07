c24217c367eb026542c2564190c8989465844551
Sammwy <sammwy.dev@gmail.com>
2022-11-07 16:24:30 -0300

    Added .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { PORT, HOST } = process.env;
+  await app.listen(PORT, HOST);
+  Logger.log(`Server listening on http://${HOST}:${PORT}`, 'NestApplication');
 }
 bootstrap();
