36d5d2883ccc28cee7c7100c1d7036ce146f4229
Sammwy <sammwy.dev@gmail.com>
2022-11-09 01:31:29 -0300

    Added validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import ValidationPipe from './pipes/validation.pipe';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(ValidationPipe);
+
   const { PORT, HOST } = process.env;
   await app.listen(PORT, HOST);
   Logger.log(`Server listening on http://${HOST}:${PORT}`, 'NestApplication');
---
+++ src/pipes/validation.pipe.ts
@@ -0,0 +1,25 @@
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe as ValidationPipeBase,
+} from '@nestjs/common';
+
+const ValidationPipe = new ValidationPipeBase({
+  forbidNonWhitelisted: true,
+  skipMissingProperties: true,
+  skipNullProperties: false,
+  skipUndefinedProperties: false,
+  stopAtFirstError: true,
+  errorHttpStatusCode: 400,
+  exceptionFactory: (errors: ValidationError[]): any => {
+    const error = errors[0];
+    const keys = Object.keys(error.constraints);
+    const firstConstraint = error.constraints[keys[0]];
+    throw new BadRequestException(
+      'INVALID_' + error.property.toUpperCase(),
+      firstConstraint,
+    );
+  },
+});
+
+export default ValidationPipe;
